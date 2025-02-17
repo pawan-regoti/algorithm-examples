dd6728e7ffa671ccd6573f0a2c5f8e77c9b368d1
Pawan Regoti <pawan.regoti@maersk.com>
2025-02-17 22:51:22 +0100

    added bfs

--- AlgorithmExamples/AlgorithmExamples/Program.cs
+++ AlgorithmExamples/AlgorithmExamples/Program.cs
@@ -1,3 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
+using AlgorithmExamples;
+using Tree.BreadthFirstSearch;
 
-Console.WriteLine("Hello, World!");
+var builder = Host.CreateApplicationBuilder(args);
+builder.Services.AddTransient<BreadthFirstSearch>();
+
+var host = builder.Build();
+var logger = host.Services.GetRequiredService<ILogger<Program>>();
+
+logger.LogInformation("Application started");
+var breadthFirstSearch = host.Services.GetRequiredService<BreadthFirstSearch>();
+
+var root = new Node(logger, "A");
+root.Neighbours.Add(new Node(logger, "B"));
+root.Neighbours.Add(new Node(logger, "C"));
+root.Neighbours[0].Neighbours.Add(new Node(logger, "D"));
+root.Neighbours[0].Neighbours.Add(new Node(logger, "E"));
+root.Neighbours[1].Neighbours.Add(new Node(logger, "F"));
+root.Neighbours[1].Neighbours.Add(new Node(logger, "G"));
+
+breadthFirstSearch.Run(root);
+logger.LogInformation("Application finished");
--- AlgorithmExamples/Graphs/BreadthFirstSearch/BreadthFirstSearch.cs
+++ AlgorithmExamples/Graphs/BreadthFirstSearch/BreadthFirstSearch.cs
@@ -1,16 +1,42 @@
+using Microsoft.Extensions.Logging;
+
 namespace Tree.BreadthFirstSearch;
 
-public class Node(string name)
+public class Node(ILogger logger,  string name)
 {
     public string Name { get; set; } = name;
-    public List<Node> Children { get; set; } = new();
-    public bool Visited { get; set; }
+    public List<Node> Neighbours { get; set; } = new();
+    public bool Visited { get; private set; }
+
+    public void Visit()
+    {
+        Visited = true;
+        logger.LogInformation("Node visited: {Name}", Name);
+    }
+
 }
 public class BreadthFirstSearch
 {
     public void Run(Node root)
     {
-        var queue = new Queue<string>();
-        
+        var queue = new Queue<Node>();
+        queue.Enqueue(root);
+
+        root.Visit();
+
+        while (queue.Count > 0)
+        {
+            var node = queue.Dequeue();
+
+            foreach (var neighbour in node.Neighbours)
+            {
+                if (!neighbour.Visited)
+                {
+                    queue.Enqueue(neighbour);
+                    neighbour.Visit();
+                }
+            } 
+        }
+
     }
 }
