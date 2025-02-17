e30e0b44c7506f9f5b81de7c54b1bd8e01885258
Pawan Regoti <pawan.regoti@maersk.com>
2025-02-17 22:21:39 +0100

    Update BreadthFirstSearch.cs

--- AlgorithmExamples/Tree/BreadthFirstSearch/BreadthFirstSearch.cs
+++ AlgorithmExamples/Tree/BreadthFirstSearch/BreadthFirstSearch.cs
@@ -1,6 +1,16 @@
 namespace Tree.BreadthFirstSearch;
 
+public class Node(string name)
+{
+    public string Name { get; set; } = name;
+    public List<Node> Children { get; set; } = new();
+    public bool Visited { get; set; }
+}
 public class BreadthFirstSearch
 {
-    
+    public void Run(Node root)
+    {
+        var queue = new Queue<string>();
+        
+    }
 }
